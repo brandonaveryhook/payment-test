2d2de01ca756aa472d2cd94be959d2c7a929eeb0
brandonaveryhook <115424384+brandonaveryhook@users.noreply.github.com>
2022-10-09 14:44:49 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-var paymentBtn = document.getElementById("payment-btn");
-
-paymentBtn.addEventListener("click", function() {
-    document.getElementById("payment-panel").hidden = true;
-    document.getElementById("message-panel").hidden = false;
-});
